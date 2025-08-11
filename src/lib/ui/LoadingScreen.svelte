bd1348e8ce9053615017005ed4b1458430de60ee
SpirioSpirio <contact@spirio.fr>
2025-08-11 18:15:28 +0200

    Change loading logo - 2

--- src/lib/ui/LoadingScreen.svelte
+++ src/lib/ui/LoadingScreen.svelte
@@ -4,6 +4,6 @@
 
 <div transition:fly|global={{y: 100, duration: 200}} class="z-50 flex h-screen">
     <div class="z-50 flex m-auto">
-        <h1 class="font-serif text-4xl font-black sm:text-6xl text-green">GeoQuest.spirio.fr</h1>
+        <h1 class="font-serif text-4xl font-black sm:text-6xl text-green">GeoQuest</h1>
     </div>
 </div>
